6d6770e6080b370b803a79ac0d3339388454a4b4
Carlos Corral <carloscorral@Carloss-MacBook-Air.local>
2016-05-14 16:14:01 -0700

    demo first commit

---
+++ client-js.js
@@ -0,0 +1,40 @@
+// import bluetooth from ('web-bluetooth');
+
+$(window).load(function() {
+  console.log(bluetooth);
+  batteryFill();
+});
+
+$('#connect').on('touchstart click', (event) => {
+  $('#load').show();
+
+  // $('#loading').show();
+  // $('#title').prepend('<p id="cancel">Cancel</p>');
+  // eddy1.discoverConnect("SAMSUNG-SM-G925A", null, null).then(device => {
+  //   $('#loading').hide();
+  //   $('#status').text('Connected!');
+  //   $('#disconnect').prop('disabled',false);
+  //   // console.log('Connected from app.js', device);
+  // }).catch(err => {
+  //   console.log(err);
+  //   $('#loading').hide();
+  // });
+});
+
+$('#cancel').on('click', event => {
+  event.preventDefault();
+  if (eddy1.disconnect()) $('#status').text('Not connected');
+});
+
+function batteryFill() {
+  $('#battery-fill').animate({
+    height: "50%"
+  },{
+    duration:1000,
+    easing:'easeOutBounce'
+  })
+}
+
+function loading() {
+
+}
