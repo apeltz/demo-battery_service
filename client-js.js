6ad978662e79175267ade8f91815a55bed428e83
Carlos Corral <carloscorral@Carloss-MacBook-Air.local>
2016-05-15 22:26:56 -0700

    Syncing up library - ready to include Monday AM

--- client-js.js
+++ client-js.js
@@ -1,34 +1,44 @@
 // const bluetooth = import 'web-bluetooth';
 
+//const blue = new Bluetooth();
+
 $(window).load(function() {
-  batteryFill();
+  batteryFill(50);
 });
 
 $('#connect').on('touchstart click', (event) => {
   $('#load').show();
-  // eddy1.discoverConnect("SAMSUNG-SM-G925A", null, null).then(device => {
-    $('#loading').hide();
+  $('#connect').prop('disabled','true');
+  // $('#disconnect').show();
+  let level = 30;
+  $('#level').text(`${level}%`);
+  // blue.acquire("SAMSUNG-SM-G925A", null, null).then(device => {
+    //$('#load').hide();
   //   $('#status').text('Connected!');
-  //   $('#disconnect').prop('disabled',false);
+    // $('#buttons').append('<p>Connected!</p>');
   //   // console.log('Connected from app.js', device);
   // }).catch(err => {
   //   console.log(err);
-  //   $('#loading').hide();
+  //   $('#load').hide();
   // });
 });
 
 $('#cancel').on('click', event => {
   event.preventDefault();
-  if (eddy1.disconnect()) $('#status').text('Not connected');
+  $('#load').hide();
+  $('#connect').show();
+  $('#disconnect').hide();
+  if (blue.disconnect()) $('#status').text('Not connected');
 });
 
-function batteryFill() {
+function batteryFill(percentage) {
   $('#battery-fill').velocity({
-    height: "50%"
+    height: `${percentage}%`
   },{
     duration:1000,
     easing:'linear'
-  })
+  });
+  // $('#battery-fill').addClass('battery-transition');
 }
 
 function loading() {
